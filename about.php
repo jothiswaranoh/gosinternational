54462b470d48b36a21fc1dbf03e733aab49b7199
Sowbaranika-G-002 <91730517+Sowbaranika-G-002@users.noreply.github.com>
2024-03-24 17:39:48 +0530

    [GI-1] Updated Who we are content

--- about.php
+++ about.php
@@ -4,184 +4,188 @@ load_template('head');
 load_template('service_header');
 ?>
 
-    <!-- Get A Quote  -->
-    <div class="modal contact-modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content margin-top-150px background-main-color">
-                <div class="row no-gutters">
-                    <div class="col-lg-5">
-                        <img src="assets/img/contact-img.jpg" alt="">
-                    </div>
-                    <div class="col-lg-7">
-                        <div class="padding-30px">
-                            <h3 class="padding-bottom-15px">Get A Free Quote</h3>
-                            <form>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label>Full Name</label>
-                                        <input type="text" class="form-control" id="inputName4" placeholder="Name">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label>Email</label>
-                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                                </div>
-                                <div class="form-group">
-                                    <label>Message</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                                <a href="#" class="btn-sm btn-lg btn-block background-dark text-white text-center  text-uppercase rounded-0 padding-15px">SEND MESSAGE</a>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- // Get A Quote  -->
-
-
-    <div class="page-title">
-        <div class="container">
-            <div class="padding-tb-120px">
-                <h1>About Us</h1>
-                <ol class="breadcrumb">
-                    <li><a href="#">Home</a></li>
-                    <li class="active">About Us</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-
-
-    <!--============= About Us =============-->
-    <div class="nile-about-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 sm-mb-45px">
-
-                    <div class="section-title-right text-main-color clearfix">
-                        <div class="icon"><img src="assets/icons/title-icon-1.png" alt=""></div>
-                        <h2 class="title-text">Who We Are ?</h2>
-                    </div>
-                    <div class="about-text margin-tb-25px">
-                        <h4>25 years of experience in Logistics services</h4>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        <br> consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ...
-                    </div>
-                    <a href="#" class="nile-bottom sm">Read More</a>
-
-                </div>
-                <div class="col-lg-6">
-                    <img src="assets/img/gritte-QUv_ahxIdPw-unsplash.jpg" width="540px" height="264px" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--============= //About Us =============-->
-
-
-
-
-
-    <div class="call-action ba-2">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 padding-tb-15px">
-                    <h2>Unbeatable Trucking and Transport Services</h2>
-                    <div class="text">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
-                </div>
+<!-- Get A Quote  -->
+<div class="modal contact-modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content margin-top-150px background-main-color">
+            <div class="row no-gutters">
                 <div class="col-lg-5">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 sm-mb-45px">
-                            <a href="#" class="action-bottom layout-1">
-								<img src="assets/icons/small-icon-1.png" alt=""> 
-								<h4>Tell Friend</h4>
-							</a>
-                        </div>
-                        <div class="col-lg-4 col-md-4 sm-mb-45px">
-                            <a href="#" class="action-bottom layout-1">
-								<img src="assets/icons/small-icon-2.png" alt=""> 
-								<h4>Read More</h4>
-							</a>
-                        </div>
-                        <div class="col-lg-4 col-md-4">
-                            <a href="#" class="action-bottom layout-1">
-								<img src="assets/icons/small-icon-3.png" alt=""> 
-								<h4>Contact Us</h4>
-							</a>
-                        </div>
+                    <img src="assets/img/contact-img.jpg" alt="">
+                </div>
+                <div class="col-lg-7">
+                    <div class="padding-30px">
+                        <h3 class="padding-bottom-15px">Get A Free Quote</h3>
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label>Full Name</label>
+                                    <input type="text" class="form-control" id="inputName4" placeholder="Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Address</label>
+                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                            </div>
+                            <div class="form-group">
+                                <label>Message</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <a href="#" class="btn-sm btn-lg btn-block background-dark text-white text-center  text-uppercase rounded-0 padding-15px">SEND MESSAGE</a>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<!-- // Get A Quote  -->
 
 
-
-
-
-    <div class="nile-about-section">
-        <div class="container">
-            <!-- Title -->
-            <div class="section-title margin-bottom-40px">
-                <div class="row justify-content-center">
-                    <div class="col-lg-7">
-                        <div class="icon text-main-color"><i class="fa fa-truck"></i></div>
-                        <div class="h2">Our Service</div>
-                        <div class="des">In quis luctus dolor. Sed ac libero arcu. Phasellus vulputate ultrices augue, eget feugiat lectus efficitur in. Nulla non pharetra justo. Nunc viverra consectetur bibendum. </div>
-                    </div>
-                </div>
-            </div>
-            <!-- // End Title -->
-
-            <div class="row">
-
-                <div class="col-lg-4">
-                    <div class="service-icon-box">
-                        <div class="icon"><img src="assets/icons/service-dark-1.png" alt=""></div>
-                        <a href="#" class="title h2">Cargo Transportation</a>
-                        <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
-                    </div>
-                    <div class="service-icon-box">
-                        <div class="icon"><img src="assets/icons/service-dark-2.png" alt=""></div>
-                        <a href="#" class="title h2">Air Freight</a>
-                        <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="text-center sm-mb-45px">
-                        <img src="assets/img/about-2.jpg" alt="" class="border-radius-500">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="service-icon-box">
-                        <div class="icon"><img src="assets/icons/service-dark-3.png" alt=""></div>
-                        <a href="#" class="title h2">Ocean Freight</a>
-                        <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
-                    </div>
-                    <div class="service-icon-box">
-                        <div class="icon"><img src="assets/icons/service-dark-4.png" alt=""></div>
-                        <a href="#" class="title h2">Packaging & Storage</a>
-                        <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
-                    </div>
-                </div>
-
-
-
-            </div>
-
-
-            <div class="text-center margin-top-35px">
-                <a href="#" class="nile-bottom md">Show all <i class="fa fa-arrow-right"></i> </a>
-            </div>
-
+<div class="page-title">
+    <div class="container">
+        <div class="padding-tb-120px">
+            <h1>About Us</h1>
+            <ol class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li class="active">About Us</li>
+            </ol>
         </div>
     </div>
+</div>
+
+
+<!--============= About Us =============-->
+<div class="nile-about-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 sm-mb-45px">
+
+                <div class="section-title-right text-main-color clearfix">
+                    <div class="icon"><img src="assets/icons/title-icon-1.png" alt=""></div>
+                    <h2 class="title-text">Who We Are ?</h2>
+                </div>
+                <div class="about-text margin-tb-25px">
+                    <h4>25 years of experience in Logistics services</h4>
+                    <p>We are leaders in the field of logistics services, having established standards for excellence in freight transportation throughout a 25-year history.
+                    Our path serves as evidence of our steadfast dedication to effectiveness, dependability, and client pleasure.
+                    We have mastered our skills by navigating through difficulties and victories, from busy ports to the most remote regions of the world.
+                    With an eye for innovation and a drive for excellence, we create custom solutions that skillfully combine knowledge, experience, and individualized attention.
+                    You can rely on us to deliver your cargo quickly and precisely, making sure it gets to its destination without incident.
+                    Discover the distinction by working with our skilled staff, who are committed to offering the best logistics solutions available.</p>
+                </div>
+                <a href="#" class="nile-bottom sm">Read More</a>
+
+            </div>
+            <div class="col-lg-6">
+                <img src="assets/img/gritte-QUv_ahxIdPw-unsplash.jpg" width="540px" height="264px" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<!--============= //About Us =============-->
+
+
+
+
+
+<div class="call-action ba-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 padding-tb-15px">
+                <h2>Unbeatable Trucking and Transport Services</h2>
+                <div class="text">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+            </div>
+            <div class="col-lg-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 sm-mb-45px">
+                        <a href="#" class="action-bottom layout-1">
+                            <img src="assets/icons/small-icon-1.png" alt="">
+                            <h4>Tell Friend</h4>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 sm-mb-45px">
+                        <a href="#" class="action-bottom layout-1">
+                            <img src="assets/icons/small-icon-2.png" alt="">
+                            <h4>Read More</h4>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <a href="#" class="action-bottom layout-1">
+                            <img src="assets/icons/small-icon-3.png" alt="">
+                            <h4>Contact Us</h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="nile-about-section">
+    <div class="container">
+        <!-- Title -->
+        <div class="section-title margin-bottom-40px">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="icon text-main-color"><i class="fa fa-truck"></i></div>
+                    <div class="h2">Our Service</div>
+                    <div class="des">In quis luctus dolor. Sed ac libero arcu. Phasellus vulputate ultrices augue, eget feugiat lectus efficitur in. Nulla non pharetra justo. Nunc viverra consectetur bibendum. </div>
+                </div>
+            </div>
+        </div>
+        <!-- // End Title -->
+
+        <div class="row">
+
+            <div class="col-lg-4">
+                <div class="service-icon-box">
+                    <div class="icon"><img src="assets/icons/service-dark-1.png" alt=""></div>
+                    <a href="#" class="title h2">Cargo Transportation</a>
+                    <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
+                </div>
+                <div class="service-icon-box">
+                    <div class="icon"><img src="assets/icons/service-dark-2.png" alt=""></div>
+                    <a href="#" class="title h2">Air Freight</a>
+                    <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="text-center sm-mb-45px">
+                    <img src="assets/img/about-2.jpg" alt="" class="border-radius-500">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="service-icon-box">
+                    <div class="icon"><img src="assets/icons/service-dark-3.png" alt=""></div>
+                    <a href="#" class="title h2">Ocean Freight</a>
+                    <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
+                </div>
+                <div class="service-icon-box">
+                    <div class="icon"><img src="assets/icons/service-dark-4.png" alt=""></div>
+                    <a href="#" class="title h2">Packaging & Storage</a>
+                    <div class="des">Ut elit tellus, luctus nec magna mattis et, pulvinar dapibus lorem leo ultricies et vitae enim.</div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+        <div class="text-center margin-top-35px">
+            <a href="#" class="nile-bottom md">Show all <i class="fa fa-arrow-right"></i> </a>
+        </div>
+
+    </div>
+</div>
 
 <?php
 load_template('footer');
