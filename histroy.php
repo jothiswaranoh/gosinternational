613bf38b952e4dc04fba1dbfa610fb898a9fb678
Sowbaranika-G-002 <91730517+Sowbaranika-G-002@users.noreply.github.com>
2024-03-26 14:28:09 +0530

    [GI-13] Organised History Files Content

--- histroy.php
+++ histroy.php
@@ -35,6 +35,7 @@ $aboutSections = array(
   ),
 );
 
+
 ?>
 <section id="call-to-action" class="call-to-action">
   <div class="container aos-init aos-animate" data-aos="zoom-out">
@@ -61,15 +62,10 @@ $aboutSections = array(
         <img src="assets/img/about-1.jpg" class="img-fluid" alt="">
       </div>
       <div class="col-md-7">
-        <h3>MISSION</h3>
+        <h3><?php echo $aboutSections[0]["title"]; ?></h3>
         <p class="fst-italic">
-          Green Ocean Shipping International aims to dominate the market in Andaman and Nicobar Islands.
+          <?php echo $aboutSections[0]["content"]; ?>
         </p>
-        <ul>
-          <li><i class="bi bi-check"></i> We're committed to improving living standards and economic stability for the people of Andaman and India.</li>
-          <li><i class="bi bi-check"></i> Our focus on reliability, satisfaction, and innovation ensures a positive impact on the shipping industry and nation-building.</li>
-          <li><i class="bi bi-check"></i> By prioritizing efficiency and results, we contribute to progress in both shipping and nation-building endeavors.</li>
-        </ul>
       </div>
     </div><!-- Features Item -->
 
@@ -78,17 +74,10 @@ $aboutSections = array(
         <img src="assets/img/about-1.jpg" class="img-fluid" alt="">
       </div>
       <div class="col-md-7 order-2 order-md-1">
-        <h3>VISION</h3>
+        <h3><?php echo $aboutSections[1]["title"]; ?></h3>
         <p class="fst-italic">
-          The goal of Green Ocean Shipping Int Pvt Ltd is to become an acknowledged authority in the shipping sector in India's Andaman and Nicobar Islands.
+          <?php echo $aboutSections[1]["content"]; ?>
         </p>
-        <ul>
-          <li><i class="bi bi-check"></i> Our aim is to provide a wide range of excellent and cutting-edge ocean-going multimodal transportation services tailored to meet our clients' evolving needs.</li>
-          <li><i class="bi bi-check"></i> We seek recognition for our steadfast dedication to quality, ethical corporate conduct, and enduring, efficient practices.</li>
-          <li><i class="bi bi-check"></i> Through customer-centric strategies and continuous improvement, we strive to become a reliable and impactful force in nation-building and initiatives like "Make in India" and "Sagar Mala."</li>
-          <li><i class="bi bi-check"></i> Our ultimate goal is to contribute to the overall development and satisfaction of our clients.</li>
-
-        </ul>
       </div>
     </div><!-- Features Item -->
 
@@ -97,18 +86,8 @@ $aboutSections = array(
         <img src="assets/img/about-1.jpg" class="img-fluid" alt="">
       </div>
       <div class="col-md-7">
-        <h3>A Tradition of Success:</h3>
-        <ul>
-          <li><i class="bi bi-check"></i>Led by visionary leaders Mr. C. Dhandapani and Mr. R. Anguraj, Srinithi Enterprises has achieved a remarkable combined turnover
-            of Rs. 32.94 crore, rooted in a rich heritage and unwavering commitment to quality under their guidance.
-          </li>
-        </ul>
-        <h3>Srinithi Enterprises' turnover:</h3>
-        <ul>
-          <li><i class="bi bi-check"></i>Srinithi Enterprises' exponential growth in the dairy and milk products sector, with turnovers of Rs. 245.04 Crores in AY 21-22, Rs. 494.16 Crores in AY 22-23, and Rs. 595.43 Crores in AY 23-24, reflects its dedication to excellence.
-            The company's strategic expansion into Agri and Agro Products, coupled with its ambitious target of reaching a Rs. 1000 Crores benchmark, underscores its strong position for continued success.
-          </li>
-        </ul>
+        <h3><?php echo $aboutSections[2]["title"]; ?></h3>
+        <p><?php echo $aboutSections[2]["content"]; ?></p>
       </div>
     </div><!-- Features Item -->
 
@@ -117,16 +96,8 @@ $aboutSections = array(
         <img src="assets/img/about-1.jpg" class="img-fluid" alt="">
       </div>
       <div class="col-md-7 order-2 order-md-1">
-        <h3>Industry Recognition:</h3>
-        <p class="fst-italic">
-          Green Ocean Shipping International Ltd has garnered accolades for its exemplary service, notably being recognized as the best shipping service during the 2005 tsunami rescue efforts and receiving appreciation from the Andaman Nicobar administration,
-          highlighting its unwavering commitment to excellence.
-        </p>
-        <h3>Robust Business Model:</h3>
-        <p>
-          Operating on a robust business model, Green Ocean Shipping actively participates in government tenders and e-auctions, offering premium shipping services including ferry and cargo transportation.
-          Its strategic approach caters to a diverse clientele, ensuring effective logistics for various large companies and government contractors.
-        </p>
+        <h3><?php echo $aboutSections[3]["title"]; ?></h3>
+        <p class="fst-italic"><?php echo $aboutSections[3]["content"]; ?></p>
       </div>
     </div><!-- Features Item -->
 
@@ -142,42 +113,11 @@ $aboutSections = array(
         <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
       </div>
       <div class="col-lg-6 content order-last  order-lg-first">
-        <h3>Green Ocean Shipping: A Legacy of Excellence in Island Shipping</h3>
-        <p>
-          Over four decades of experience, unwavering dedication, and a commitment to sustainability define Green Ocean Shipping International Pvt. Ltd. (GOSI).
-          Our journey began in 1977 as Meenakshi Traders, a testament to the vision of our founder, Mr. M. Subramaniam.
-          Since then, we've grown from a local ferry service in the Andaman and Nicobar Islands to a trusted leader in island shipping. </p>
-        <ul>
-          <li data-aos="fade-up" data-aos-delay="100">
-            <i class="bi bi-diagram-3"></i>
-            <div>
-              <h5>A Tradition of Innovation and Sustainability</h5>
-              <p>
-                Green Ocean Shipping isn't just about transporting people and cargo. We're passionate about innovation and environmental responsibility.
-                We strive for excellence in our services while maintaining a clean and green ocean, adhering to the highest international standards.</p>
-            </div>
-          </li>
-          <li data-aos="fade-up" data-aos-delay="200">
-            <i class="bi bi-fullscreen-exit"></i>
-            <div>
-              <h5>Our Evolution</h5>
-          <li><i class="bi bi-check"></i> 1977: Founded as Meenakshi Traders, marking our commitment to the shipping industry in the Andaman and Nicobar Islands..</li>
-          <li><i class="bi bi-check"></i> 2013: Evolved into Green Ocean Shipping, ushering in a new era of excellence in ferry services and cargo transportation.</li>
-          <li><i class="bi bi-check"></i> Today: Green Ocean Shipping International Pvt. Ltd. stands as a symbol of dedication, innovation, and a commitment to a sustainable future.
-            Third-Generation Leadership</li>
+        <h3><?php echo $aboutSections[4]["title"]; ?></h3>
+        <p><?php echo $aboutSections[4]["content"]; ?></p>
+
       </div>
-      </li>
-      <li data-aos="fade-up" data-aos-delay="300">
-        <i class="bi bi-broadcast"></i>
-        <div>
-          <h5>"A Legacy of Excellence: Tracing the Roots of Srinithi"</h5>
-          <p>Srinithi Enterprises Pvt Ltd, formerly known as Dhanalakshmi Traders since 1978, has undergone a transformative journey in the dairy industry. From its roots as Kamadhenu Dairy Farm to its rebranding as Srinidhi Milk Foods in 2003, the company has continuously evolved, showcasing unwavering dedication to innovation and sustainability.
-            Today, as Srinithi Enterprises Pvt Ltd, it stands as a beacon of excellence, embodying a legacy built on commitment, innovation, and superior quality in the dairy sector.</p>
-        </div>
-      </li>
-      </ul>
     </div>
-  </div>
 
   </div>
 </section>
