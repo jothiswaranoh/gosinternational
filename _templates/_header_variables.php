989e8ebc22ebb02c1cfb4fc99e509bf3766d52c6
Sowbaranika-G-002 <91730517+Sowbaranika-G-002@users.noreply.github.com>
2024-03-27 12:59:08 +0530

    [GI-17] Made a separate file for header variables

---
+++ _templates/_header_variables.php
@@ -0,0 +1,19 @@
+<?php
+// Header variabless
+$cargo_cart_title = "Cargo Transportation";
+$cargo_delivery_amnt = "1 X 250$";
+
+$air_freight_cart_title = "Air Freight";
+$air_freight_delivery_amnt = "1 X 250$";
+
+$pack_store_title = "Packaging & Storage";
+$pack_store_amnt = "1 X 250$";
+
+$sub_total_title = "Subtotal:";
+$sub_total_amnt = "$71.00";
+
+$view_cart = "View Cart";
+$checkout = "Checkout";
+
+$get_a_quote = "Get A Quote";
+?>
