d16ce4bb2873c341b832ea268a91e81ad3744278
jothiswaran <72430882+jothiswaranoh@users.noreply.github.com>
2024-03-27 14:37:48 +0530

    Update _service_header.php

--- _templates/_service_header.php
+++ _templates/_service_header.php
@@ -30,7 +30,7 @@ $socialMediaLinks = array(
                     <div class="position-relative">
                         <div class="row">
                             <div class="col-lg-3 col-md-12">
-                                <a id="logo" href="index.php" class="d-inline-block margin-tb-15px"><img src="assets/img/logo-dark.png" alt=""></a>
+                                <a id="logo" href="index.php" class="d-inline-block margin-tb-15px w-25"><img src="assets/img/logo-1.png" alt=""></a>
                                 <a class="mobile-toggle padding-15px background-second-color border-radius-3" href="#"><i class="fa fa-bars"></i></a>
                             </div>
                             <div class="col-lg-7 col-md-12 position-inherit">
@@ -41,63 +41,6 @@ $socialMediaLinks = array(
                                             </a></li>
                                     <?php endforeach; ?>
                                 </ul>
-
-                                <div class="d-none d-xl-block pull-right model-link margin-top-15px">
-                                    <a id="cart-link" class="model-link margin-right-25px text-white opacity-hover-8" href="#">
-                                        <span>3</span><i class="fa fa-shopping-cart"></i>
-                                    </a>
-                                    <div class="cart-model">
-                                        <div class="media">
-                                            <a href="#">
-                                                <img class="d-flex align-self-start mr-3 width-50px" src="assets/img/cart-1.jpg" alt="Generic placeholder image">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#" class="close"><i class="fa fa-window-close"></i></a>
-                                                <h5 class="mt-0"><a href="#">Cargo Transportation</a></h5>
-                                                <h6 class="mt-0 font-2">1 X 250$</h6>
-                                            </div>
-                                        </div>
-
-                                        <div class="media">
-                                            <a href="#">
-                                                <img class="d-flex align-self-start mr-3 width-50px" src="assets/img/cart-2.jpg" alt="Generic placeholder image">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#" class="close"><i class="fa fa-window-close"></i></a>
-                                                <h5 class="mt-0"><a href="#">Air Freight</a></h5>
-                                                <h6 class="mt-0 font-2">1 X 250$</h6>
-                                            </div>
-                                        </div>
-
-                                        <div class="media">
-                                            <a href="#">
-                                                <img class="d-flex align-self-start mr-3 width-50px" src="assets/img/cart-3.jpg" alt="Generic placeholder image">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#" class="close"><i class="fa fa-window-close"></i></a>
-                                                <h5 class="mt-0"><a href="#">Packaging & Storage</a></h5>
-                                                <h6 class="mt-0 font-2">1 X 250$</h6>
-                                            </div>
-                                        </div>
-
-                                        <div class="padding-0px">
-                                            <div class="cart-total background-main-color padding-lr-30px padding-tb-10px text-white">
-                                                <div class="desc">Subtotal:</div>
-                                                <div class="price"><strong>$71.00</strong></div>
-                                                <div class="clearfix"></div>
-                                            </div>
-                                            <a href="#" class="btn btn-primary btn-lg btn-block">view
-                                                cart</a>
-                                            <a href="#" class="btn btn-outline-primary btn-lg btn-block">Checkout</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-none d-xl-block search-link pull-right model-link margin-top-15px">
-                                    <a id="search-header" class="model-link margin-right-0px text-white opacity-hover-8" href="#search">
-                                        <i class="fa fa-search"></i>
-                                    </a>
-                                </div>
-
                             </div>
                             <div class="col-lg-2 col-md-12  d-none d-lg-block">
                                 <a data-toggle="modal" data-target=".bd-example-modal-lg" href="#" class="btn btn-sm border-radius-30 margin-tb-20px text-white  background-main-color  box-shadow float-right padding-lr-20px margin-left-30px d-block">
