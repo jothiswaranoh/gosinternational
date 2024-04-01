71a00113e81e4b80853b042390008af230e95d5d
Sowbaranika-G-002 <91730517+Sowbaranika-G-002@users.noreply.github.com>
2024-04-01 21:22:14 +0530

    [GI-29] Modified ocean freight contents in home page

--- _templates/_slider.php
+++ _templates/_slider.php
@@ -2,34 +2,27 @@
   <div class="row gap-16px">
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix">
+        <div class="clearfix d-flex justify-content-center align-items-center">
           <div class="icon"><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">Ocean Freight</a>
-        </div>
-        <div class="dis clearfix">
-          <p class="margin-top-16px">"Beyond Boundaries, Together."</p>
+          <a href="#" class="title">Navigating Horizons, Bridging Worlds, Forging Limitless Connections </a>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix">
-          <div class="icon"><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">Ocean Freight</a>
-        </div>
-        <div class="dis clearfix">
-          <p class="margin-top-16px">"Navigating Horizons, Bridging Worlds."</p>
+        <div class="clearfix d-flex justify-content-center align-items-center">
+          <div class="icon "><img src="assets/icons/service-light-3.png" alt=""></div>
+          <a href="#" class="title">
+            <div class="clearfix d-flex justify-content-around align-items-center">OCEAN FREIGHT</div><br>
+          The backbone of global exchange, empowering connections worldwide.</a>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix">
+        <div class="clearfix d-flex justify-content-center align-items-center">
           <div class="icon"><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">Ocean Freight</a>
-        </div>
-        <div class="dis clearfix">
-          <p class="margin-top-16px">"The backbone of global exchange."</p>
+          <a href="#" class="title">Breaking Barriers, Building Bridges, Achieving Together, Uniting Beyond Limits</a>
         </div>
       </div>
     </div>
