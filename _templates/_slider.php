38fb633d282a9120b8faf63d4af764347856d011
JayaSamuthraDevi <jeevitham811@gmail.com>
2024-04-02 07:06:18 +0530

    corrected service box

--- _templates/_slider.php
+++ _templates/_slider.php
@@ -2,27 +2,32 @@
   <div class="row gap-16px">
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix d-flex justify-content-center align-items-center">
+        <div class="clearfix d-flex flex-column justify-content-center align-items-center">
           <div class="icon"><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">Navigating Horizons, Bridging Worlds, Forging Limitless Connections </a>
+          <p class="title clearfix mb-0">Navigating Horizons, Bridging Worlds, Forging Limitless Connections </p>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix d-flex justify-content-center align-items-center">
+        <div class="clearfix d-flex flex-column justify-content-center align-items-center">
           <div class="icon "><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">
-            <div class="clearfix d-flex justify-content-around align-items-center">OCEAN FREIGHT</div><br>
-          The backbone of global exchange, empowering connections worldwide.</a>
+          <div class=" title clearfix d-flex flex-column justify-content-center align-items-center">
+            <p>OCEAN FREIGHT</p>
+            <p class="mb-0">
+              The backbone of global exchange, empowering connections worldwide.
+            </p>
+          </div>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="service layout-1">
-        <div class="clearfix d-flex justify-content-center align-items-center">
+        <div class="clearfix d-flex flex-column justify-content-center align-items-center">
           <div class="icon"><img src="assets/icons/service-light-3.png" alt=""></div>
-          <a href="#" class="title">Breaking Barriers, Building Bridges, Achieving Together, Uniting Beyond Limits</a>
+          <p class="title clearfix mb-0">
+            Breaking Barriers, Building Bridges, Achieving Together, Uniting Beyond Limits
+          </p>
         </div>
       </div>
     </div>
